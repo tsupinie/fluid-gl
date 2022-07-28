76d76a2fa1ba4d09356c9554ac1d593fd12acefd
Tim Supinie <tsupinie@gmail.com>
2022-07-27 23:16:57 -0500

    Extract texture rgb to the relevant variables

--- js/main.js
+++ js/main.js
@@ -288,6 +288,8 @@ class ShallowWaterSolver extends WebGLEntity {
             highp vec2 jhat = vec2(0., u_unit.y);
 
             highp vec3 tex, tex_ip1, tex_im1, tex_jp1, tex_jm1;
+            highp vec2 wind, wind_ip1, wind_im1, wind_jp1, wind_jm1;
+            highp float hght, hght_ip1, hght_im1, hght_jp1, hght_jm1;
 
             if (u_istage == 0) {
                 tex = texture2D(u_stage0_sampler, v_tex_coord).rgb;
@@ -304,17 +306,26 @@ class ShallowWaterSolver extends WebGLEntity {
                 tex_jm1 = texture2D(u_stage1_sampler, v_tex_coord - jhat).rgb;
             }
 
-            highp vec3 dtex_dx = (tex_ip1 - tex_im1) / (2. * u_dx);
-            highp vec3 dtex_dy = (tex_jp1 - tex_jm1) / (2. * u_dx);   
-            highp vec3 d2tex_dx2 = (tex_ip1 - 2. * tex + tex_im1) / (4. * u_dx * u_dx);
-            highp vec3 d2tex_dy2 = (tex_jp1 - 2. * tex + tex_jm1) / (4. * u_dx * u_dx);
+            hght = tex.r;
+            hght_ip1 = tex_ip1.r; hght_im1 = tex_im1.r; hght_jp1 = tex_jp1.r; hght_jm1 = tex_jm1.r;
+
+            wind = tex.gb;
+            wind_ip1 = tex_ip1.gb; wind_im1 = tex_im1.gb; wind_jp1 = tex_jp1.gb; wind_jm1 = tex_jm1.gb;
+
+            highp float dz_dx = (hght_ip1 - hght_im1) / (2. * u_dx);
+            highp float dz_dy = (hght_jp1 - hght_jm1) / (2. * u_dx);
+
+            highp vec2 dwind_dx = (wind_ip1 - wind_im1) / (2. * u_dx);
+            highp vec2 dwind_dy = (wind_jp1 - wind_jm1) / (2. * u_dx);
+            highp vec2 d2wind_dx2 = (wind_ip1 - 2. * wind + wind_im1) / (4. * u_dx * u_dx);
+            highp vec2 d2wind_dy2 = (wind_jp1 - 2. * wind + wind_jm1) / (4. * u_dx * u_dx);
 
             // Apply Neumann BC for height
             if (v_tex_coord.x - ihat.x < 0. || v_tex_coord.x + ihat.x > 1.) {
-                dtex_dx.r = 0.;
+                dz_dx = 0.;
             }
             if (v_tex_coord.y - jhat.y < 0. || v_tex_coord.y + jhat.y > 1.) {
-                dtex_dy.r = 0.;
+                dz_dy = 0.;
             }
 
             highp vec3 dtex_dt = vec3(0., 0., 0.);
@@ -322,9 +333,8 @@ class ShallowWaterSolver extends WebGLEntity {
             highp float mean_depth = 2.;
             highp float kinematic_viscosity = 5e-2;
 
-            dtex_dt.r = -((mean_depth + tex.r) * dtex_dx.g + tex.g * dtex_dx.r + (mean_depth + tex.r) * dtex_dy.b + tex.b * dtex_dy.r);
-            dtex_dt.g = -9.806 * dtex_dx.r - tex.g * dtex_dx.g - tex.b * dtex_dy.g + kinematic_viscosity * (d2tex_dx2.g + d2tex_dy2.g);
-            dtex_dt.b = -9.806 * dtex_dy.r - tex.g * dtex_dx.b - tex.b * dtex_dy.b + kinematic_viscosity * (d2tex_dx2.b + d2tex_dy2.b);
+            dtex_dt.r = -((mean_depth + hght) * dwind_dx.x + wind.x * dz_dx + (mean_depth + hght) * dwind_dy.y + wind.y * dz_dy);
+            dtex_dt.gb = -9.806 * vec2(dz_dx, dz_dy) - wind.x * dwind_dx - wind.y * dwind_dy + kinematic_viscosity * (d2wind_dx2 + d2wind_dy2);
 
             if (u_istage == 0) {
                 highp vec3 out_tex = tex + u_dt * dtex_dt;
