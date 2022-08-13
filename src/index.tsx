667fbe6080afda73223c4424df8c4654cb93714a
Tim Supinie <tsupinie@gmail.com>
2022-08-12 21:51:25 -0500

    Typescript conversion

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
 import './index.css';
 import ShallowWaterApp from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <ShallowWaterApp />
-    </React.StrictMode>
-);
+const root_elem = document.getElementById('root');
+
+if (root_elem !== null) {
+    const root = ReactDOM.createRoot(root_elem);
+    root.render(
+        <React.StrictMode>
+            <ShallowWaterApp />
+        </React.StrictMode>
+    );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
