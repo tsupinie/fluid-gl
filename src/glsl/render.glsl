dce213bf7ea7fad779cc253c7ec7e392f6f33b57
Tim Supinie <tsupinie@gmail.com>
2022-08-15 18:43:13 -0500

    Tweak height coloring

--- src/glsl/render.glsl
+++ src/glsl/render.glsl
@@ -5,15 +5,15 @@ uniform sampler2D u_colormap_sampler;
 varying highp vec2 v_tex_coord;
 varying highp vec2 v_state_coord;
 
-highp float cmap_min = -0.1;
-highp float cmap_max = 0.1;
+highp float cmap_min = -1.;
+highp float cmap_max = 1.;
 
 void main() {
     highp float dot_pos = length(v_tex_coord - vec2(0.5, 0.5));
 
     if (dot_pos > 0.5) discard;
 
-    highp float logistic_k = 8.;
+    highp float logistic_k = 50.;
     highp float logistic_max = 1.;
 
     highp vec3 tex = texture2D(u_sampler, v_state_coord).rgb;
